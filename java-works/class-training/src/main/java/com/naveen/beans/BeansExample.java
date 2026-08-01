19fd31a5d522e2ddf3f399344d3e2ca0b5c4ef7b
Naveen <adith.naveen@gmail.com>
2026-08-01 13:38:19 +0530

    updated files with method and test case

--- java-works/class-training/src/main/java/com/naveen/beans/BeansExample.java
+++ java-works/class-training/src/main/java/com/naveen/beans/BeansExample.java
@@ -1,6 +1,9 @@
 package com.naveen.beans;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class Customer {
     private int custId;
     private String custFirstName;
@@ -27,12 +30,39 @@ class Customer {
         return this.income;
     }
     // set - only write
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+    public void setCustFirstName (String custFirstName) {
+        this.custFirstName  = custFirstName;
+    }
 
+    public void setCustLastName(String custLastName) {
+        this.custLastName = custLastName;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    @Override
+    public String toString() {
+//        return super.toString();
+        return this.custId +", " + this.custFirstName +", " +this.custLastName +", " + this.income;
+    }
 }
 
 public class BeansExample {
+    private static final Logger log = LoggerFactory.getLogger(BeansExample.class);
+
     public static void main(String[] args) {
         Customer customer = new Customer();
-        customer.
+        customer.setCustId(101);
+        customer.setCustFirstName("Naveen");
+        customer.setCustLastName("Kumar");
+        customer.setIncome(1234);
+
+        log.info("customer info {}", customer.toString());
+        log.info("customer info {}", customer);
     }
 }
