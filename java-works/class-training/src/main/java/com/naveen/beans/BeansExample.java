ca3b643a16a643983bf9dff4895fbfb61bc12715
Naveen <adith.naveen@gmail.com>
2026-08-01 13:49:28 +0530

    updated with bean class get and set

--- java-works/class-training/src/main/java/com/naveen/beans/BeansExample.java
+++ java-works/class-training/src/main/java/com/naveen/beans/BeansExample.java
@@ -4,53 +4,124 @@ package com.naveen.beans;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// defn of bean
+// a bean a class which has private variable
+// public getters and setters
+
+class Name  {
+    private String firstName;
+    private String midName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMidName() {
+        return midName;
+    }
+
+    public void setMidName(String midName) {
+        this.midName = midName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Name{" +
+                "firstName='" + firstName + '\'' +
+                ", midName='" + midName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
+
+
+
 class Customer {
     private int custId;
-    private String custFirstName;
-    private String custLastName;
     private double income;
+    private Name name;
 
-// every class shall have default constructor by compiler at the time of compiling
-    // if you have not created one
-
-    // get - only read
-
-    public int getCustId() {
-        return this.custId;
+    public Name getName() {
+        return name;
     }
 
-    public String getCustFirstName() {
-        return this.custFirstName;
+    public void setName(Name name) {
+        this.name = name;
     }
 
-    public String getCustLastName() {
-        return this.custLastName;
-    }
-    public double getIncome () {
-        return this.income;
-    }
-    // set - only write
-    public void setCustId(int custId) {
-        this.custId = custId;
-    }
-    public void setCustFirstName (String custFirstName) {
-        this.custFirstName  = custFirstName;
-    }
-
-    public void setCustLastName(String custLastName) {
-        this.custLastName = custLastName;
+    public double getIncome() {
+        return income;
     }
 
     public void setIncome(double income) {
         this.income = income;
     }
 
+    public int getCustId() {
+        return custId;
+    }
+
+    public void setCustId(int custId) {
+        this.custId = custId;
+    }
+
+// every class shall have default constructor by compiler at the time of compiling
+    // if you have not created one
+
+
     @Override
     public String toString() {
-//        return super.toString();
-        return this.custId +", " + this.custFirstName +", " +this.custLastName +", " + this.income;
+        return "Customer{" +
+                "custId=" + custId +
+                ", custFirstName='" + custFirstName + '\'' +
+                ", custLastName='" + custLastName + '\'' +
+                ", income=" + income +
+                '}';
     }
 }
+
+class Employee  {
+    private int empId;
+    private  Name name;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", name=" + name +
+                '}';
+    }
+}
+
 
 public class BeansExample {
     private static final Logger log = LoggerFactory.getLogger(BeansExample.class);
@@ -58,10 +129,24 @@ public class BeansExample {
     public static void main(String[] args) {
         Customer customer = new Customer();
         customer.setCustId(101);
-        customer.setCustFirstName("Naveen");
-        customer.setCustLastName("Kumar");
+        Name name = new Name();
+
+        name.setFirstName("Naveen");
+        name.setMidName("Kumar");
+        name.setLastName("Srinivas");
+
+        customer.setName(name);
         customer.setIncome(1234);
 
+        Employee employee = new Employee();
+        employee.setEmpId(101);
+        Name name1 = new Name();
+
+        name1.setFirstName("Harish");
+        name1.setMidName("Kumar");
+        name1.setLastName("Vishal");
+
+        employee.setName(name);
         log.info("customer info {}", customer.toString());
         log.info("customer info {}", customer);
     }
