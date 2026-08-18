c4847f0f40e38e5dbbff02bcf34933f4d71c0d5b
Naveen <adith.naveen@gmail.com>
2026-08-18 11:31:03 +0530

    working on CompileTime polymorphism

---
+++ java-works/class-training/src/main/java/com/naveen/polymorphism/CompileTime.java
@@ -0,0 +1,53 @@
+package com.naveen.polymorphism;
+
+public class CompileTime {
+
+    public static void display() {
+        System.out.println("Display Called without any parameters");
+    }
+//
+//    public static void display(String name) {
+//        System.out.println("Display with String Name : " + name);
+//    }
+
+    public static void display(int value) {
+        System.out.println("Display with Int Value " + value);
+    }
+
+    public static void display(String name, int value) {
+        System.out.println("Display Name " + name +" value : " + value);
+    }
+//
+//    public static void display(String carType, String [] cars) {
+//        System.out.println("Car Type " + carType);
+//
+//        // or
+//        for(String car : cars) {
+//            System.out.println(car);
+//        }
+//    }
+
+
+// the variable arguments can take array or any values, but if you use variable arguments then it should be the last one
+    public static  void display(String carType, String ... cars ) {
+        System.out.println("Car Type " + carType);
+
+        // or
+        for(String car : cars) {
+            System.out.println(car);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        display();
+        display("Vineeth");
+        display(123);
+        display("Vineeth", 333);
+
+        display("suv", new String[]{"BMW X1", "Hyundai Creta", "Jeep Compasss"});
+        display("suv");
+
+        display("suv",  "BMW X3", "Hyundai Venue", "Volvo XC90" );
+    }
+}
